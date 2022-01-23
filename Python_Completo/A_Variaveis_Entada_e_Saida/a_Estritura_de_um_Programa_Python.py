2eea4de57d1576d8461fee28232d90366565622b
YgorDaniel <ygordaniel55@gmail.com>
2022-01-23 20:47:50 -0300

    Estrutura de um Programa

---
+++ Python_Completo/A_Variaveis_Entada_e_Saida/a_Estritura_de_um_Programa_Python.py
@@ -0,0 +1,25 @@
+#Estrutura de um Programa
+'''É comum na progração iniciarmos nosso codigo pelo método/função main. 
+No nosso curso utilizaremos a estrutura abaixo:'''
+ 
+def main():
+    #comandos
+    pass
+#-------------
+# A linha a seguir executa o programa
+main()
+
+
+# Entendendo a estrutua
+''' O python funciona por identação. Execulta primeiro o mais a esquerda possivel de cima para baixo'''
+
+# Escopo pai
+    # Escopo filho
+        # Escopo neto
+            #Escopo bisneto
+
+# def -> Define uma função
+
+# () -> Chama uma função
+
+# : -> Declara o início de novo nível de escopo
