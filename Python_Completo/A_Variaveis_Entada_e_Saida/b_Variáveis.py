b97110e56eb87a4f632e4d53859108eb3cb204aa
YgorDaniel <ygordaniel55@gmail.com>
2022-01-23 21:56:41 -0300

    Variáveis

---
+++ Python_Completo/A_Variaveis_Entada_e_Saida/b_Variáveis.py
@@ -0,0 +1,33 @@
+# Variáveis
+'''Uma variável é um nome que se refere a um valor,
+são usadas para guardarmos valores que serão usados no programa''' 
+
+# Atribuição -> =
+'''Comando de atribuição cria uma nova variavel e lhe dá um valor'''
+# Exemplo
+primeiro_numero = 15
+segundo_numero = 10
+soma = primeiro_numero + segundo_numero
+
+print( "Soma =", soma ) 
+
+primeiro_numero = 20
+segundo_numero = 25
+soma = soma + primeiro_numero + segundo_numero
+
+print( "Soma =", soma ) 
+
+#Exercicio 2.1
+''' Dado dois numeros a e b calcule a soma '''
+a = 10
+b = 35
+soma = a + b
+
+#Exercicio 2.2
+''' Dado dois numeros a e b calcule a multiplicação'''
+a = 5
+b = 9
+multiplicacao = a * b
+
+print("Soma =", soma)
+print("multiplicação =", multiplicacao)
