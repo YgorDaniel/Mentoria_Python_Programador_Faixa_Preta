2722a485aadddf50be4766ded238c934510e1599
YgorDaniel <ygordaniel55@gmail.com>
2022-01-24 00:44:38 -0300

    Tipos de Variável

---
+++ Python_Completo/A_Variaveis_Entada_e_Saida/f_Tipos_de_Variável.py
@@ -0,0 +1,75 @@
+# Tipos de Variável
+
+'''
+str -> String texto
+int -> Número inteiro
+float -> Número decimal
+
+'''
+print(type(23))
+print(type("23"))
+
+print("###############################") 
+# int -> Número inteiro
+def main():
+    a = int(input("Digite o primeiro numero "))
+    b = int(input("Digite o segundo numero "))
+    soma = a + b
+
+    print("Tipo de a:", type(a))
+    print("Tipo de b:", type(b))
+    print("Soma =",soma)
+
+main() 
+
+print("###############################") 
+
+# float -> Número decimal
+def main():
+    a = float(input("Digite o primeiro numero "))
+    b = float(input("Digite o segundo numero "))
+    soma = a + b
+
+    print("Tipo de a:", type(a))
+    print("Tipo de b:", type(b))
+    print("Soma =",soma)
+
+main() 
+
+#Exercicio 2.3
+print("###############################") 
+print("Exercicio 2.3") 
+print("Fazer uma função que multiplique dois numeros entrados pelo usuário")
+def main():
+    a = float(input("Digite o primeiro numero "))
+    b = float(input("Digite o segundo numero "))
+    multiplicacao = a * b
+
+    print("Multiplicação =",multiplicacao)
+main()
+
+ # Exercicio 2.4
+print("###############################") 
+print("Exercicio 2.4")  
+print("Fazer uma função que divida dois numeros entrados pelo usuário")
+def main():
+    a = float(input("Digite o primeiro numero "))
+    b = float(input("Digite o segundo numero "))
+    divisao = a / b
+
+    print("Divisão =",divisao)
+main() 
+
+# Exercicio 2.5
+print("###############################") 
+print("Exercicio 2.5")
+print("Fazer uma função que subtraia quatro numeros entrados pelo usuário")
+def main():
+    a = float(input("Digite o primeiro numero "))
+    b = float(input("Digite o segundo numero "))
+    c = float(input("Digite o terceiro numero "))
+    d = float(input("Digite o quarto numero "))
+    subtracao = a - b - c -d
+
+    print("Subtração =",subtracao)
+main() 
