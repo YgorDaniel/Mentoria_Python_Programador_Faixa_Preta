89a733894a8034124c38327a3bebfaf976736147
YgorDaniel <ygordaniel55@gmail.com>
2022-01-23 23:24:32 -0300

    Output

---
+++ Python_Completo/A_Variaveis_Entada_e_Saida/d_Saida_de_Dados.py
@@ -0,0 +1,13 @@
+# Métodos de saidas de dados (Output)
+# Print() -> função utilizada para enviamos output para o usuário
+
+def main():
+    a = 1
+    b = 4
+    soma = a + b
+
+    print("Soma =",soma)
+
+main()  
+   
+ 
